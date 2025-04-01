2d6251cf110105528aeba66a4ddeed37dc6bf49c
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-04-01 22:17:44 +0100

    Latest Commit

---
+++ src/schema/vendor.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, SchemaFactory,Schema } from "@nestjs/mongoose";
+import { UUID } from "mongodb";
+import {Document} from "mongoose";
+@Schema()
+export class Vendor extends Document{
+    @Prop({required:true,unique:true})//Give each vendor their unique id
+    id:UUID
+    @Prop({required:true})
+    firstname:string
+    @Prop({required:true})
+    lastname:string
+    @Prop({required:true})
+    phonenumber:string
+    @Prop({required:true})
+    password:string
+    @Prop()
+    isVerified:boolean
+    @Prop()
+    isActive:boolean
+    @Prop()
+    createdAt:Date
+    @Prop({required:true})
+    updatedAt:boolean
+    @Prop({ default: 'vendor' })  // Ensure role is always set to vendor
+    role: string;
+}
