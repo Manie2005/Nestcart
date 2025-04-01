3030be5ff7f770d539d106497a9a4a3717fc19f5
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-04-01 22:00:04 +0100

    Latest Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VendorModule } from './vendor/vendor.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [VendorModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
