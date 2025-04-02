f44f9fd909e56a6a9f21f892eb9eb1027a053283
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-04-02 05:57:33 +0100

    Latest Commit

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VendorService } from './vendor.service';
 import { VendorController } from './vendor.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { VendorSchema } from 'src/schema/vendor.schema';
 @Module({
+  imports:[MongooseModule.forFeature([{name:'Vendor',schema:VendorSchema}])],
   providers: [VendorService],
-  controllers: [VendorController]
+  controllers: [VendorController],
+  exports:[VendorService]
 })
 export class VendorModule {}
--- src/vendor/vendor.service.ts
+++ src/vendor/vendor.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Vendor } from 'src/schema/vendor.schema';
 
 @Injectable()
-export class VendorService {}
+export class VendorService {
+constructor(@InjectModel(Vendor.name) private readonly vendorModel: Model<Vendor>,
+){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
