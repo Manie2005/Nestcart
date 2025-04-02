bd9a60e895a709b4e91d840e5b2253fe0bf6b410
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-04-02 08:29:07 +0100

    Updated Commit

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -4,11 +4,21 @@ import { VendorController } from './vendor.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VendorSchema } from 'src/schema/vendor.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { MailerModule, MailerService } from '@nestjs-modules/mailer';
 @Module({
   imports:[MongooseModule.forFeature([{name:'Vendor',schema:VendorSchema}]),
   JwtModule.register({
     secret:process.env.JWT_SECRET_KEY ||'candace',
     signOptions:{expiresIn:'1h'},
+  }),
+  MailerModule.forRoot({
+    transport:{
+      service:'smtp.gmail.com',
+      auth:{
+        user:process.env.EMAIL_USER,
+        pass:process.env.EMAIL_PASS
+      }
+    }
   })
 ],
   providers: [VendorService],
