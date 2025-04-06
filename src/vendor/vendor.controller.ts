3dc2d8839ef0c3328c4c55fd5703736e5c34b1ae
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-04-06 14:15:55 +0100

    Latest Commit

--- src/vendor/vendor.controller.ts
+++ src/vendor/vendor.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { VendorService } from './vendor.service';
 import { CreateVendorDto } from 'src/dto/create-vendor.dto';
+import { LoginDto } from 'src/dto/login-vendor.dto';
 
 @Controller('vendor')
 export class VendorController {
     constructor (private  readonly vendorService:VendorService){}
+
 @Post('signup')
 async signup(@Body() createVendorDto:CreateVendorDto){
     return this.vendorService.signup(createVendorDto);
 }
+@Post('login')
+async login(@Body() loginDto:LoginDto){
+    const{ email,password }=loginDto;
+    return this.vendorService.login(loginDto);
+}
+
 
 }
