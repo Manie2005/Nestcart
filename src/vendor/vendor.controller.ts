e10b127c48f9b0cfda99340ff7be5e93a5de2bc5
Your Name~Manie2005 <maniesamuel24@gmail.com>
2025-04-06 14:25:00 +0100

    Updated Commit

--- src/vendor/vendor.controller.ts
+++ src/vendor/vendor.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { VendorService } from './vendor.service';
 import { CreateVendorDto } from 'src/dto/create-vendor.dto';
 import { LoginDto } from 'src/dto/login-vendor.dto';
+import { VerifyOtpDto } from 'src/dto/verify-otp.dto';
 
 @Controller('vendor')
 export class VendorController {
@@ -13,9 +14,24 @@ async signup(@Body() createVendorDto:CreateVendorDto){
 }
 @Post('login')
 async login(@Body() loginDto:LoginDto){
-    const{ email,password }=loginDto;
+    const {email,password }=loginDto;
     return this.vendorService.login(loginDto);
 }
+@Post('verify-otp')
+async verifyOtp(@Body()verifyOtpDto:VerifyOtpDto){
+const {email,otpCode} =verifyOtpDto;
+try{
+const isVerified = await this.vendorService.verifyOtp(verifyOtpDto);
+if(isVerified){
+    return{message:'User Successfully Verified'};
+    }
+    else{
+        throw new BadRequestException('Invalid OTP');
+    }
+}catch(error){
 
+}
+
+}
 
 }
