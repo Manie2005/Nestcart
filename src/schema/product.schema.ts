2c91dc46c2d2d78f62f8ec49fff8927e376525a1
Immanuel <maniesamuel24@gmail.com>
2025-05-12 20:56:50 +0100

    Updated commit

--- src/schema/product.schema.ts
+++ src/schema/product.schema.ts
@@ -7,6 +7,21 @@ export class Product extends Document {
   @Prop()
   title: string;
 
+  @Prop()
+  description: string;
+
+  @Prop()
+  price: string;
+
+  @Prop()
+  quantity: string;
+
+  @Prop()
+  imageUrl: string;
+
+  @Prop()
+  category: string;
+
   @Prop({ type: Types.ObjectId, ref: 'Vendor' })
   vendor: Types.ObjectId; // Many-to-one: Product belongs to one Vendor
 }
