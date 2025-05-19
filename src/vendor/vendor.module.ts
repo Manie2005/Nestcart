156e846af0c6659572673cc58797c44968c18642
Immanuel <maniesamuel24@gmail.com>
2025-05-19 16:46:33 +0100

    Fixed error

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -3,10 +3,16 @@ import { VendorController } from './vendor.controller';
 import { VendorService } from './vendor.service';
 import { Vendor, VendorSchema } from 'src/schema/vendor.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Vendor.name, schema: VendorSchema }])
+    MongooseModule.forFeature([{ name: Vendor.name, schema: VendorSchema }]),
+    JwtModule.register({
+secret:jwtConstants.secret || 'candace' ,
+signOptions:{expiresIn:'120s'},
+    }),
   ],
   controllers: [VendorController],
   providers: [VendorService]
